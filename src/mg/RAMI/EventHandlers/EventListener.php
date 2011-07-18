c7ac988beb81144e298ba4c33c869c97cfb4cb76
Marcelo Gornstein <marcelog@gmail.com>
2011-07-18 12:26:02 -0300

    added generic event handler

---
+++ src/mg/RAMI/EventHandlers/EventListener.php
@@ -0,0 +1,40 @@
+<?php
+namespace EventHandlers;
+
+class EventListener extends PDOListener
+{
+    /**
+     * @var \PDOStatement
+     */
+    private $_insertStatement;
+
+    /**
+     * @var \PDOStatement
+     */
+    private $_createStatement;
+
+    public function setCreateStatement($statement)
+    {
+        $this->_createStatement = $statement;
+    }
+
+    public function setInsertStatement($statement)
+    {
+        $this->_insertStatement = $statement;
+    }
+
+    public function onAnyEvent($event)
+    {
+        $this->executeStatement($this->_insertStatement, array(
+            'name' => $event->getName(),
+            'event' => serialize($event)
+        ));
+    }
+
+    public function init()
+    {
+        $this->executeStatement($this->_createStatement, array());
+    }
+}
+
+
