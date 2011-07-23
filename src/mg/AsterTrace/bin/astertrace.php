2b725afb14fc05239d861d4586dc671907880772
Marcelo Gornstein <marcelog@gmail.com>
2011-07-23 17:48:48 -0300

    added apache license 2, commented files

--- src/mg/AsterTrace/bin/astertrace.php
+++ src/mg/AsterTrace/bin/astertrace.php
@@ -1,4 +1,32 @@
 <?php
+/**
+ * Main entry point. Will bootstrap, instantiate the container and
+ * start things up.
+ *
+ * PHP Version 5
+ *
+ * @category AsterTrace
+ * @package  Bin
+ * @author   Marcelo Gornstein <marcelog@gmail.com>
+ * @license  http://marcelog.github.com/ Apache License 2.0
+ * @version  SVN: $Id$
+ * @link     http://marcelog.github.com/
+ *
+ * Copyright 2011 Marcelo Gornstein <marcelog@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
 // Main Entry Point
