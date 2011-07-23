4dbdb29d2d08d73791159782222f38550f07d429
Marcelo Gornstein <marcelog@gmail.com>
2011-07-23 16:58:53 -0300

    added varset handlers

---
+++ src/mg/RAMI/EventHandlers/VarSetListener.php
@@ -0,0 +1,42 @@
+<?php
+namespace EventHandlers;
+
+class VarSetListener extends PDOListener
+{
+    /**
+     * @var \PDOStatement
+     */
+    private $_insertStatement;
+
+    /**
+     * @var \PDOStatement
+     */
+    private $_createStatement;
+
+    public function setCreateStatement($statement)
+    {
+        $this->_createStatement = $statement;
+    }
+
+    public function setInsertStatement($statement)
+    {
+        $this->_insertStatement = $statement;
+    }
+
+    public function onVarSet($event)
+    {
+        $this->executeStatement($this->_insertStatement, array(
+            'value' => $event->getValue(),
+            'uniqueid' => $event->getUniqueId(),
+            'channel' => $event->getChannel(),
+            'name' => $event->getVariableName()
+        ));
+    }
+
+    public function init()
+    {
+        $this->executeStatement($this->_createStatement, array());
+    }
+}
+
+
