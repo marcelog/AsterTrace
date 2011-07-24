69de5dc06e6772155c69a7cd82b1c2d569fe9469
Marcelo Gornstein <marcelog@gmail.com>
2011-07-24 17:10:24 -0300

    added newchannel monitor

---
+++ src/mg/AsterTrace/EventHandlers/NewChannelListener.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * NewChannel listener, will capture all incoming calls.
+ *
+ * PHP Version 5
+ *
+ * @category AsterTrace
+ * @package  EventHandlers
+ * @author   Marcelo Gornstein <marcelog@gmail.com>
+ * @license  http://marcelog.github.com/ Apache License 2.0
+ * @version  SVN: $Id$
+ * @link     http://marcelog.github.com/
+ *
+ * Copyright 2011 Marcelo Gornstein <marcelog@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+namespace AsterTrace\EventHandlers;
+
+class NewChannelListener extends PDOListener
+{
+    public function onNewchannel($event)
+    {
+        $this->executeStatement($this->insertStatement, array(
+            'uniqueid' => $event->getUniqueId(),
+            'channel' => $event->getChannel(),
+            'exten' => method_exists($event, 'getExtension') ? $event->getExtension() : '',
+            'context' => method_exists($event, 'getContext') ? $event->getContext() : '',
+            'state' => $event->getChannelState(),
+            'description' => $event->getChannelStateDesc(),
+            'clid_num' => $event->getCallerIdNum(),
+            'clid_name' => $event->getCallerIdName()
+        ));
+    }
+}
+
